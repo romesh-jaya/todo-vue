264448b38f4d124acb4487b34e2093fca626720b
romesh-jaya <56665179+romesh-jaya@users.noreply.github.com>
2021-12-23 10:01:48 +0530

    feat: add ts support

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from "vue";
 import TodoList from "./components/TodoList.vue";
 import EditTodo from "./components/EditTodo.vue";
